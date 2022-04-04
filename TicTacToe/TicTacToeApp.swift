f4e58d1c02272ad434efbb26b5c329ff3bdf05b9
Maxime Point <maximepoint6@gmail.com>
2022-04-04 09:09:09 +0200

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Maxime Point on 04/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
